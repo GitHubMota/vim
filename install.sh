a382b659794d269679f5dde9def59687bbf2680c
Mota <motablow@gmail.com>
2017-04-04 16:06:39 +0800

    Auto config shell script upload again.

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
+if which apt-get >/dev/null; then
+    sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
+elif which yum >/dev/null; then
+    sudo yum install -y gcc vim git ctags xclip astyle python-setuptools python-devel   
+fi
+
+##Add HomeBrew support on  Mac OS
+if which brew >/dev/null;then
+    echo "You are using HomeBrew tool"
+    brew install vim ctags git astyle
+fi
+
+sudo easy_install -ZU autopep8 
+if [ ! -x /usr/local/bin/ctags ];then
+    sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
+fi
+
+if [ ! -f "_vimrc" ] || [ ! -f "Tomorrow-Night-Eighties.vim" ];then
+    echo "git clone _vimrc and Tomorrow-Night-Eighties.vim from github."
+    rm -rf ./vim
+    git clone https://github.com/GitHubMota/vim.git
+    mv vim/_vimrc .
+    mv vim/Tomorrow-Night-Eighties.vim .
+fi
+
+if [ -f ~/.vimrc ];then
+    echo "move old .vimrc to .vimrc_old"
+    mv ~/.vimrc ~/.vimrc_old
+elif [ -L ~/.vimrc ];then
+    echo "del old .vimrc link"
+    rm ~/.vimrc
+fi
+ln -s $PWD/_vimrc ~/.vimrc
+if [ ! -d ~/.vim/bundle/vundle ];then
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
+
+mkdir -p ~/.vim/colors
+cp Tomorrow-Night-Eighties.vim ~/.vim/colors/
+
+echo "ma6174正在努力为您安装bundle程序" > ma6174
+echo "安装完毕将自动退出" >> ma6174
+echo "请耐心等待" >> ma6174
+vim ma6174 -c "BundleInstall" -c "q" -c "q"
+rm ma6174
+echo "安装完成"
